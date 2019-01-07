d8a81efafd976afc3a1bedaab3920f7e4562ee63
Stewart ilondanga <stilondanga@gmail.com>
2019-01-07 22:55:45 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,32 @@ var navigate = (function() {
     $('.' + dataName).toggle();
   });
 })();
-/*
+
 $(document).ready(function() {
 
-    $('.search').each(function() {
-        var self = $(this);
-        var div = self.children('div');
-        var placeholder = div.children('input').attr('placeholder');
-        var placeholderArr = placeholder.split(/ +/);
-        if(placeholderArr.length) {
-            var spans = $('<div />');
-            $.each(placeholderArr, function(index, value) {
-                spans.append($('<span />').html(value + '&nbsp;'));
-            });
-            div.append(spans);
-        }
-        self.click(function() {
-            self.addClass('open');
-            setTimeout(function() {
-                self.find('input').focus();
-            }, 750);
-        });
-        $(document).click(function(e) {
-            if(!$(e.target).is(self) && !jQuery.contains(self[0], e.target)) {
-                self.removeClass('open');
-            }
-        });
+  $('.search').each(function() {
+    var self = $(this);
+    var div = self.children('div');
+    var placeholder = div.children('input').attr('placeholder');
+    var placeholderArr = placeholder.split(/ +/);
+    if (placeholderArr.length) {
+      var spans = $('<div />');
+      $.each(placeholderArr, function(index, value) {
+        spans.append($('<span />').html(value + '&nbsp;'));
+      });
+      div.append(spans);
+    }
+    self.click(function() {
+      self.addClass('open');
+      setTimeout(function() {
+        self.find('input').focus();
+      }, 750);
     });
+    $(document).click(function(e) {
+      if (!$(e.target).is(self) && !jQuery.contains(self[0], e.target)) {
+        self.removeClass('open');
+      }
+    });
+  });
 
 });
